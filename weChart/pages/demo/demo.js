cdacfae08e1e35dedda4a29cfc72e9bdba04d4e1
dlongh <du_long_hui.com>
2018-09-18 11:20:54 +0800

    demo提交

--- weChart/pages/demo/demo.js
+++ weChart/pages/demo/demo.js
@@ -1,7 +1,11 @@
 Page({
   data: {
     text: "hello word",
-    array: [{msg:1} , {msg:2}]
+    subtext: "hello dulonghui",
+    num: 0,
+    array: [{ text: "init data" }],
+    // array: [{msg:1} , {msg:2}]
+
   },
   onLoad: function(){
 
@@ -24,15 +28,26 @@ Page({
   onShow: function(){
     console.log(this.route);
   },
-  setData: function() {
-    this.setData({
-      text: 'changed data'
-    })
-  },
   changeText: function () {
     // this.data.text = 'changed data'  // bad, it can not work
     this.setData({
       text: 'changed data'
     })
   },
+  setNum: function(){
+    this.data.num = 1
+    this.setData({
+      num: this.data.num
+    })
+  },
+  getArray: function(){
+    this.setData({
+      'array[0].text': 'change data'
+    })
+  },
+  addNewField: function(){
+    this.setData({
+      'newField.text' : 'new data'
+    })
+  }
 })
